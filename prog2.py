de894a5441feffe4281444e2a33577e8244aea2b
Rosemary Amorim <mary_rose752@hotmail.com>
2023-06-17 12:11:03 -0300

    Atualização

--- prog2.py
+++ prog2.py
@@ -1,3 +1,7 @@
-print("-=" * 10)
-print("Cadastro Clientes")
-print("-=" * 10)
+#FAÇA UM PROGRAMA QUE RECEBA UM NÚMERO, SEU SUCESSOR E SEU ANTECESSOR
+num = int(input("Digite um número: "))
+ant = num - 1
+print("Seu antecessor é: ", ant)
+suc = num + 1
+print("Seu sucessor é: ", suc)
+
